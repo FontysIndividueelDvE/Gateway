48b92938991a42e84a82a7da38dd23b05c6e7a56
DennisvEngelen <344709@student.fontys.nl>
2024-04-08 16:11:01 +0200

    walking skeleton

--- src/main/java/IndividueelDVE/Gateway/config/GatewayConfig.java
+++ src/main/java/IndividueelDVE/Gateway/config/GatewayConfig.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.RouteMatcher;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
@@ -17,22 +19,20 @@ public class GatewayConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfigurationSource source = request -> {
             CorsConfiguration corsConfiguration = new CorsConfiguration();
-            corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-            corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+            corsConfiguration.addAllowedOrigin(("http://localhost:3000"));
+            corsConfiguration.addAllowedMethod("*");
+            corsConfiguration.addAllowedHeader("*");
             return corsConfiguration;
         };
         return new CorsWebFilter(source);
     }
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route( r -> r
-                        .path("/get")
-                        .filters(f->f.addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
-                .route("auth-service", r -> r.path("/auth/**")
-                        .uri("lb://auth-service"))
-                .build();
+            .route("authenticationservice", r -> r
+                    .path("/test/**")
+                    .uri("http://localhost:8081"))
+            .build();
     }
 }
